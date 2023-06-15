4b9c0189877a5c0389ebcf0ec012e8b253bf7b2f
Erin Keller <aestheticartist@gmail.com>
2023-06-15 15:27:24 -0400

    Added comments to service worker file

--- client/sw.js
+++ client/sw.js
@@ -1,49 +1,50 @@
+// initialize variables
 import { registerRoute } from 'workbox-routing';
-import { CacheFirst, StaleWhileRevalidate } from 'workbox-strategies';
-import { precacheAndRoute } from 'workbox-precaching';
+import { CacheFirst, StaleWhileRevalidate } from 'workbox-strategies'; 
+import { precacheAndRoute } from 'workbox-precaching'; 
 import { CacheableResponsePlugin } from 'workbox-cacheable-response';
-import { ExpirationPlugin } from 'workbox-expiration';
+import { ExpirationPlugin } from 'workbox-expiration'; 
 
-// Precache and route the assets
+// precache and route the assets
 precacheAndRoute(self.__WB_MANIFEST);
 
-// Cache the HTML pages
+// cache the HTML pages
 const pageCache = new CacheFirst({
-  cacheName: 'page-cache',
+  cacheName: 'jate-page-cache', // specify the name of the cache
   plugins: [
     new CacheableResponsePlugin({
-      statuses: [0, 200],
+      statuses: [0, 200], // cache responses with status codes 0 and 200
     }),
     new ExpirationPlugin({
-      maxAgeSeconds: 30 * 24 * 60 * 60,
+      maxAgeSeconds: 30 * 24 * 60 * 60, // set the maximum age of cached responses to 30 days
     }),
   ],
 });
 
-// Warm the cache with specific URLs
+// warm the cache with specific URLs
 registerRoute(
-  ({ request }) => request.mode === 'navigate',
-  pageCache
+  ({ request }) => request.mode === 'navigate', // match requests with navigation mode (e.g., HTML pages)
+  pageCache // use the pageCache strategy to handle matched requests
 );
 
-// Register the route for scripts, styles, and fonts
+// register the route for scripts, styles, and fonts
 registerRoute(
   ({ request }) =>
-    request.destination === 'script' ||
-    request.destination === 'style' ||
-    request.destination === 'font',
-  pageCache
+    request.destination === 'script' || // match requests with destination 'script'
+    request.destination === 'style' || // match requests with destination 'style'
+    request.destination === 'font', // match requests with destination 'font'
+  pageCache // use the pageCache strategy to handle matched requests
 );
 
-// Set up asset cache
+// set up asset cache
 registerRoute(
   ({ request }) =>
-    ['style', 'script', 'worker'].includes(request.destination),
+    ['style', 'script', 'worker'].includes(request.destination), // match requests with destination 'style', 'script', or 'worker'
   new StaleWhileRevalidate({
-    cacheName: 'asset-cache',
+    cacheName: 'jate-asset-cache', // specify the name of the cache for assets
     plugins: [
       new CacheableResponsePlugin({
-        statuses: [0, 200],
+        statuses: [0, 200], // cache responses with status codes 0 and 200
       }),
     ],
   })
